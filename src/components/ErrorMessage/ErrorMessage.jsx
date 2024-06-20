55bf56a235f2e75b8ec79a6ace5783e4b99535d7
Artem Lysiak <tiomalysiak@gmail.com>
2024-06-20 15:09:30 +0200

    fix img

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,15 +1,11 @@
 import styles from "./ErrorMessage.module.css";
 import PropTypes from "prop-types";
+import oopsImage from "../../assets/oopsImage.png";
 
 const ErrorMessage = ({ message }) => {
   return (
     <div className={styles.errorMessage}>
-      <img
-        src="/src/assets/oopsImage.png"
-        alt="Oops"
-        width="300"
-        height="300"
-      />
+      <img src={oopsImage} alt="Oops" width="300" height="300" />
       <p>{message}</p>
     </div>
   );
